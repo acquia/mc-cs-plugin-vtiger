404318da700ceef0d56946e3be761c04d8a1e61c
Jan Kozak <galvani78@gmail.com>
2018-10-08 15:03:49 +0200

    fix empty request if no mapping set

--- Sync/EventSyncService.php
+++ Sync/EventSyncService.php
@@ -64,6 +64,11 @@ final class EventSyncService
     public function sync(?\DateTimeInterface $dateFrom, ?\DateTimeInterface $dateTo)
     {
         $mapping = $this->leadEventSupplier->getLeadsMapping();
+        
+        if (!count($mapping)) {
+            DebugLogger::log(VtigerCrmIntegration::NAME, 'No mapped contacts to synchronize activities for.');;
+            return;
+        }
 
         $this->settingProvider->exceptConfigured();
 
