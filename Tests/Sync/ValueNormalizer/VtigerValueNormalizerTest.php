94f552fb5b33a10cd750d00e8440f04bebfd3fe7
Don Gilbert <don@dongilbert.net>
2020-01-06 15:47:09 -0500

    Fix tests

--- Tests/Sync/ValueNormalizer/VtigerValueNormalizerTest.php
+++ Tests/Sync/ValueNormalizer/VtigerValueNormalizerTest.php
@@ -22,7 +22,7 @@ use MauticPlugin\MauticVtigerCrmBundle\Vtiger\Model\ModuleFieldInfo;
 use MauticPlugin\MauticVtigerCrmBundle\Vtiger\Repository\Direction\LeadFieldDirection;
 use MauticPlugin\MauticVtigerCrmBundle\Vtiger\Type\CommonType;
 
-class VtigerValueNormalizerTest extends \PHPUnit_Framework_TestCase
+class VtigerValueNormalizerTest extends \PHPUnit\Framework\TestCase
 {
     /**
      * @var VtigerValueNormalizer
