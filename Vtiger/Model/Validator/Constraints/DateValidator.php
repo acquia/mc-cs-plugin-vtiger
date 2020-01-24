f5b747d6bf93f5ac482fe88a2031a5008ae80496
Lukas Sykora <lukas.sykora@acquia.com>
2020-01-24 17:14:21 +0100

    MAUT-3257 / buildViolation on context

--- Vtiger/Model/Validator/Constraints/DateValidator.php
+++ Vtiger/Model/Validator/Constraints/DateValidator.php
@@ -2,6 +2,7 @@
 
 namespace MauticPlugin\MauticVtigerCrmBundle\Vtiger\Model\Validator\Constraints;
 
+use DateTime;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
@@ -20,24 +21,28 @@ class DateValidator extends ConstraintValidator
     public function validate($value, Constraint $constraint)
     {
         if (!$constraint instanceof Date) {
-            throw new UnexpectedTypeException($constraint, __NAMESPACE__ . '\Date');
+            throw new UnexpectedTypeException($constraint, Date::class);
         }
 
+        // custom constraints should ignore null and empty values to allow
+        // other constraints (NotBlank, NotNull, etc.) take care of that
         if (null === $value || '' === $value) {
             return;
         }
 
+        // check if item can be converted to string
         if (!is_scalar($value) && !(is_object($value) && method_exists($value, '__toString'))) {
             throw new UnexpectedTypeException($value, 'string');
         }
 
-        $date         = \DateTime::createFromFormat('Y-m-d', $value);
+        $date = DateTime::createFromFormat('Y-m-d', $value);
 
         if (!$date || ($date->format('Y-m-d') != $value)) {
             $this->context->buildViolation($constraint->message)
-                          ->setParameter('{{ value }}', $this->formatValue($value))
-                          ->setCode(\Symfony\Component\Validator\Constraints\Date::INVALID_DATE_ERROR)
-                          ->addViolation();
+                ->setParameter('{{ value }}', $this->formatValue($value))
+                ->setCode(\Symfony\Component\Validator\Constraints\Date::INVALID_DATE_ERROR)
+                ->addViolation();
         }
     }
 }
+
