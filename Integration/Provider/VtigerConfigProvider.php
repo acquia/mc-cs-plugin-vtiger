efb73730f3bf54f09e17e711123eec76a97f0c48
Lukas Sykora <lukas.sykora@acquia.com>
2020-02-07 14:25:14 +0100

    MAUT-3280-supported-featues-no-flip

--- Integration/Provider/VtigerConfigProvider.php
+++ Integration/Provider/VtigerConfigProvider.php
@@ -68,7 +68,7 @@ class VtigerConfigProvider implements ConfigFormInterface, ConfigFormSyncInterfa
     public function getSupportedFeatures(): array
     {
         return [
-            'mautic.integration.feature.sync' => ConfigFormFeaturesInterface::FEATURE_SYNC,
+            ConfigFormFeaturesInterface::FEATURE_SYNC => 'mautic.integration.feature.sync',
             //ConfigFormFeaturesInterface::FEATURE_PUSH_ACTIVITY => 'mautic.integration.feature.push_activity',
         ];
     }
